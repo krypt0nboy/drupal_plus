7eb74b0684b53df91ab1892a3f44d9162578b66c
kryptonboy <kryptonb0y@harold-cohen.com>
2016-10-12 12:14:34 +0200

    Module update

--- drupal_plus.api.php
+++ drupal_plus.api.php
@@ -27,3 +27,58 @@ function hook_cache_info() {
 
   return $info;
 }
+
+/**
+ * Declares a permission template.
+ *
+ * @return array
+ *   An associative array where the keys are the template name and the values are :
+ *    - tpl : The permission template.
+ *    - title : The permission title.
+ *    - description : The permission description.
+ */
+function hook_permission_tpl_info() {
+  $info                               = array();
+  $info['%op%.%scope%.%arg1%.%arg2%'] = array(
+    'tpl'         => '%op%.%scope%.%arg1%.%arg2%',
+    'components'  => array(
+      'op'    => 'some op',
+      'scope' => 'some scope',
+      'arg1'  => 'foo',
+      'arg2'  => 'bar',
+    ),
+    'title'       => 'A title',
+    'description' => 'A description.',
+    'group'       => 'group'
+  );
+  $info['add_any_bundle']             = array(
+    'tpl'         => 'add.any.%bundle%.%entity_type%',
+    'components'  => array(
+      'op'          => 'add',
+      'scope'       => 'any',
+      'bundle'      => '%bundle%',
+      'entity_type' => '%entity_type%',
+    ),
+    'title'       => 'add any %bundle% %entity_type%',
+    'description' => 'Add any %bundle% %entity_type%',
+    'group'       => 'entities'
+  );
+
+  return $info;
+}
+
+
+/**
+ * Declares settings variables.
+ *
+ * @return array
+ */
+function hook_settings_var_info() {
+  $info     = array();
+  $info[''] = array(
+    'type'    => '',
+    'default' => ''
+  );
+
+  return $info;
+}
